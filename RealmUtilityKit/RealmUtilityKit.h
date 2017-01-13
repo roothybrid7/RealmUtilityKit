98dc5785563e66f24a001a73bff49bb5cb7b6ff3
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-13 16:48:24 +0900

    Added a Xcode project

---
+++ RealmUtilityKit/RealmUtilityKit.h
@@ -0,0 +1,19 @@
+//
+//  RealmUtilityKit.h
+//  RealmUtilityKit
+//
+//  Created by SATOSHI OHKI on 1/13/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmUtilityKit.
+FOUNDATION_EXPORT double RealmUtilityKitVersionNumber;
+
+//! Project version string for RealmUtilityKit.
+FOUNDATION_EXPORT const unsigned char RealmUtilityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmUtilityKit/PublicHeader.h>
+
+
